4238559e9d4f6779c59284a4e705f0355a247aff
Terramora <40336791+Terramora@users.noreply.github.com>
2021-06-27 18:01:59 +0300

    6*
    Загрузка динамического контента из json

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -118,6 +118,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+FIXTURE_DIRS = (
+    BASE_DIR / 'products/fixtures',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from geekshop.settings import BASE_DIR
+from json import load
+from pathlib import Path
 
 
 # Create your views here.
@@ -9,4 +12,12 @@ def index(requests):
 
 
 def products(requests):
-    return render(requests, 'products/products.html')
+    """
+    :param requests:
+    :return:
+    """
+    # сработал только абсолютный путь
+    with open('C:\\Users\\Terramora.CNV\\Documents\\GitHub\\Django\\geekshop\\products\\fixtures\\products.json', 'r',
+              encoding='utf-8') as f:
+        context = load(f)
+        return render(requests, 'products/products.html', context)
