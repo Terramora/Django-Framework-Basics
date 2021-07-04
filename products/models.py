c59d27b8ca2f289cecf644f2e528c1f8f0a3f6e3
Terramora <40336791+Terramora@users.noreply.github.com>
2021-07-04 20:26:50 +0300

    add media

--- products/models.py
+++ products/models.py
@@ -7,9 +7,8 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True, null=True)
 
-    class Meta:
-        app_label = 'products'
-        managed = True
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -20,6 +19,5 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
-    class Meta:
-        app_label = 'products'
-        managed = True
+    def __str__(self):
+        return f'{self.name} || {self.category.name}'
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 from json import load
 from datetime import datetime
 
@@ -16,8 +17,11 @@ def products(requests):
     :param requests:
     :return:
     """
-    with open(BASE_DIR / 'products/fixtures/products.json', 'r', encoding='utf-8') as f:
-        context = load(f)
-        context['year'] = datetime.now().year
-        context['title'] = 'Products'
-        return render(requests, 'products/products.html', context)
+    # with open(BASE_DIR / 'products/fixtures/products.json', 'r', encoding='utf-8') as f:
+    #     context = load(f)
+    context = {'products': Product.objects.all(),
+               'categories': ProductCategory.objects.all(),
+               'year': datetime.now().year,
+               'title': 'Products'}
+    print(context)
+    return render(requests, 'products/products.html', context)
