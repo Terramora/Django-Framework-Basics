61d3f4feb416d127584d4443c797b2844cf4f601
Terramora <40336791+Terramora@users.noreply.github.com>
2021-07-04 20:57:18 +0300

    clear code

--- products/views.py
+++ products/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from products.models import Product, ProductCategory
-from json import load
 from datetime import datetime
 
+
 # Create your views here.
-from geekshop.settings import BASE_DIR
 
 
 def index(requests):
@@ -17,11 +16,8 @@ def products(requests):
     :param requests:
     :return:
     """
-    # with open(BASE_DIR / 'products/fixtures/products.json', 'r', encoding='utf-8') as f:
-    #     context = load(f)
     context = {'products': Product.objects.all(),
                'categories': ProductCategory.objects.all(),
                'year': datetime.now().year,
                'title': 'Products'}
-    print(context)
     return render(requests, 'products/products.html', context)
