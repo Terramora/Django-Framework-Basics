9486db89875247e580dc9a76da2f83faf05c3716
Terramora <40336791+Terramora@users.noreply.github.com>
2021-07-07 22:34:36 +0300

    add registration, logout, login, print error in class="alert alert-warning alert-dismissible fade show"

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import login, registrations
+from users.views import login, registrations, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registrations/', registrations, name='registrations')
+    path('registrations/', registrations, name='registrations'),
+    path('logout/', logout, name='logout')
 ]
