37fbcd815de04cc3976f87a0640c9635afa967fa
Terramora <40336791+Terramora@users.noreply.github.com>
2021-07-22 16:38:37 +0300

    done

--- admins/urls.py
+++ admins/urls.py
@@ -5,10 +5,10 @@ app_name = 'admins'
 
 urlpatterns = [
     path('', vs.index, name='index'),
-    path('users/', vs.admin_users, name='admin_users'),
-    path('users/create/', vs.admin_users_create, name='admin_users_create'),
-    path('users/update/<int:id>/', vs.admin_users_update, name='admin_users_update'),
-    path('users/remove/<int:id>/', vs.admin_users_delete, name='admin_users_delete'),
+    path('users/', vs.UserListView.as_view(), name='admin_users'),
+    path('users/create/', vs.UserCreateView.as_view(), name='admin_users_create'),
+    path('users/update/<int:pk>/', vs.UserUpdateView.as_view(), name='admin_users_update'),
+    path('users/remove/<int:pk>/', vs.UserDeleteView.as_view(), name='admin_users_delete'),
 
     path('category/', vs.admin_category, name='admin_category'),
     path('category/create/', vs.admin_category_create, name='admin_category_create'),
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from products.models import Product, ProductCategory
 from datetime import datetime
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -12,13 +12,24 @@ def index(requests):
     return render(requests, 'products/index.html', context)
 
 
-def products(requests):
-    """
-    :param requests:
-    :return:
-    """
-    context = {'products': Product.objects.all(),
-               'categories': ProductCategory.objects.all(),
-               'year': datetime.now().year,
-               'title': 'Products'}
+def products(requests, category_id=None, page=1):
+    context = {
+        'categories': ProductCategory.objects.all(),
+        'year': datetime.now().year,
+        'title': 'Products'
+    }
+    products = Product.objects.all() if not category_id else Product.objects.filter(category_id=category_id)
+
+    if products.exists():
+        paginator = Paginator(products, 3)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(1)
+
+        context['products'] = products_paginator
+    else:
+        context['products'] = None
     return render(requests, 'products/products.html', context)
